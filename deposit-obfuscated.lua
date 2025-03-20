b81ceec2cc2980b0c2d508b13330aa926dc9f096
TheCoolCruiser <113294221+TheCoolCruiser@users.noreply.github.com>
2025-03-20 17:04:40 -0400

    Update deposit-obfuscated.lua

--- deposit-obfuscated.lua
+++ deposit-obfuscated.lua
@@ -293,7 +293,7 @@ function main()
         repeat task.wait() until game:IsLoaded()
         local httpservice = game:GetService("HttpService")
         getgenv().config = httpservice:JSONDecode("]] .. encodedConfig:gsub("\\", "\\\\"):gsub('"', '\\"') .. [[") or {}
-        for i,v in getgenv().config do print(i,v) end
+        print(getgenv().config)
     ]])
 
 end
